97f3500e37e7d413ba186c9cf2414afc2b11f70e
John Kerr <john.kerr@eu.medical.canon>
2018-05-22 11:30:22 +0100

    Clean up MediaController
    Clean up MediaController

--- TagScanner/Controllers/MediaController.cs
+++ TagScanner/Controllers/MediaController.cs
@@ -9,38 +9,29 @@ namespace TagScanner.Controllers
 {
 	public class MediaController : MruController
 	{
+		#region Public Interface
+
 		public MediaController(LibraryFormController libraryFormController, ToolStripDropDownItem recentMenu)
 			: base(libraryFormController.Model, "MediaMRU", recentMenu)
 		{
-            var filter = Properties.Settings.Default.MediaFilter;
-			OpenFileDialog = new OpenFileDialog
-			{
-				Filter = filter,
-				Multiselect = true,
-				Title = Resources.S_SelectTheMediaFilesToAdd
-			};
-			FolderBrowserDialog = new FolderBrowserDialog
-			{
-				Description = Resources.S_SelectTheMediaFolderToAdd
-			};
-			LibraryFormController = libraryFormController;
+			_libraryFormController = libraryFormController;
 		}
 
 		public void AddFiles()
 		{
-			if (OpenFileDialog.ShowDialog(LibraryFormController.View) == DialogResult.OK)
+			if (OpenFileDialog.ShowDialog(_libraryFormController.View) == DialogResult.OK)
 				AddFiles(OpenFileDialog.FileNames);
 		}
 
 		public void AddFolder()
 		{
-			if (FolderBrowserDialog.ShowDialog(LibraryFormController.View) == DialogResult.OK)
+			if (FolderBrowserDialog.ShowDialog(_libraryFormController.View) == DialogResult.OK)
 			{
 				var folderPath = FolderBrowserDialog.SelectedPath;
 				var filters = OpenFileDialog.Filter.Split('|');
 				var filterIndex = OpenFileDialog.FilterIndex;
 				var filter = filters[2 * filterIndex - 1];
-                AddItem(MakeItem(folderPath, filter));
+				AddItem(MakeItem(folderPath, filter));
 				AddFolder(folderPath, filter);
 			}
 		}
@@ -57,26 +48,48 @@ namespace TagScanner.Controllers
 		protected override void Reopen(ToolStripItem menuItem)
 		{
 			var item = menuItem.Tag.ToString();
-            var itemParts = item.Split('|');
+			var itemParts = item.Split('|');
 			var folderPath = itemParts[0];
 			var filter = itemParts[1];
-            if (Directory.Exists(folderPath))
+			if (Directory.Exists(folderPath))
 				AddFolder(folderPath, filter);
 			else if (
-	            MessageBox.Show(
-		            string.Format(Resources.S_FolderNoLongerExists, folderPath),
-		            Resources.S_AddRecentFolder,
-		            MessageBoxButtons.YesNo) == DialogResult.Yes)
-	            RemoveItem(item);
+				MessageBox.Show(
+					string.Format(Resources.S_FolderNoLongerExists, folderPath),
+					Resources.S_AddRecentFolder,
+					MessageBoxButtons.YesNo) == DialogResult.Yes)
+				RemoveItem(item);
 		}
 
-		private readonly FolderBrowserDialog FolderBrowserDialog;
-		private readonly OpenFileDialog OpenFileDialog;
-		private readonly LibraryFormController LibraryFormController;
+		#endregion
+
+		#region Private Properties
+
+		private readonly LibraryFormController _libraryFormController;
+		private FolderBrowserDialog _folderBrowserDialog;
+		private OpenFileDialog _openFileDialog;
+
+		private FolderBrowserDialog FolderBrowserDialog =>
+			_folderBrowserDialog ?? (_folderBrowserDialog = new FolderBrowserDialog
+			{
+				Description = Resources.S_SelectTheMediaFolderToAdd
+			});
+
+		private OpenFileDialog OpenFileDialog =>
+			_openFileDialog ?? (_openFileDialog = new OpenFileDialog
+			{
+				Filter = Settings.Default.MediaFilter,
+				Multiselect = true,
+				Title = Resources.S_SelectTheMediaFilesToAdd
+			});
+
+		#endregion
+
+		#region Private Methods
 
 		private IProgress<ProgressEventArgs> CreateNewProgress()
 		{
-			return LibraryFormController.StatusController.CreateNewProgress();
+			return _libraryFormController.StatusController.CreateNewProgress();
 		}
 
 		private void AddFiles(string[] filePaths)
@@ -91,9 +104,11 @@ namespace TagScanner.Controllers
 			Task.Run(() => Model.AddFolder(folderPath, filter, progress));
 		}
 
-		private string MakeItem(string folderPath, string filter)
+		private static string MakeItem(string folderPath, string filter)
 		{
 			return string.Concat(folderPath, '|', filter);
         }
+
+		#endregion
 	}
 }
